dcd1b6df25c7b55d56e95f6090980fba9258e1b2
DanSotoC <49667594+DanSotoC@users.noreply.github.com>
2020-08-20 22:37:51 -0400

    update

--- functions/main.js
+++ functions/main.js
@@ -7,12 +7,42 @@ const config = {
     messagingSenderId: "402489516954",
     appId: "1:402489516954:web:0b5a4ab87127931c501023",
     measurementId: "G-D5ES7N2D5B"
-}
+};
 
 
-firabase.initializeApp(config);
+firebase.initializeApp(config);
 
 const firestore = firebase.firestore();
 
 const crear_dueño_form = document.querySelector("#crear_dueño_form");
 const guardar_dueño_btn = document.querySelector("#guardar_dueño_btn");
+
+if (crear_dueño_form != null){
+    let d;
+    crear_dueño_form.addEventListener("submit", async(e)=>{ 
+        e.preventDefault();
+        if(document.getElementById("nombre_dueño").value != "" && document.getElementById("apellido_dueño").value != ""){
+            let nombre = document.getElementById("nombre_dueño").value;
+            let apellido = document.getElementById("apellido_dueño").value;
+            let telefono = document.getElementById("telefono_dueño").value;
+            let descripcion = document.getElementById("descripcion_dueño").value;
+        
+            let post = {
+                nombre,
+                apellido,
+                telefono,
+                descripcion
+            }
+
+            await firebase.firestore().collection("Dueños").add(post);
+            console.log("Se ha creado el dueño correctamente...");
+
+
+        }
+        else{
+            console.log("Debe completar los primeros 2 campos...");
+        }
+        
+        
+    })
+}
