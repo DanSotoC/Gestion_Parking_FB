d5d9e144cecc9832318c6a8688be786853a31da0
DanSotoC <danielsotocorrea21@hotmail.com>
2020-08-30 19:26:25 -0400

    Se agregan Barras de Busquedas

--- functions/main.js
+++ functions/main.js
@@ -1,3 +1,4 @@
+//Conexion a la base de datos - Firebase
 const config = {
     apiKey: "AIzaSyDInPMKYhERb27BCD3GLseikaxjQoiCi9I",
     authDomain: "gestion-parking-tal.firebaseapp.com",
@@ -9,21 +10,32 @@ const config = {
     measurementId: "G-D5ES7N2D5B"
 };
 
-
+// Se inicia bd
 firebase.initializeApp(config);
 
 
 const firestore = firebase.firestore();
 const crear_dueño_form = document.getElementById("crear_dueño_form");
 const ingreso_form = document.getElementById("ingreso_form");
+const buscar_dueño_form = document.getElementById("buscar_dueño_form");
+const buscar_salida_form = document.getElementById("buscar_salida_form");
 const Vehiculos_Container = document.getElementById("vehiculos-container");
+const Dueños_Container = document.getElementById("dueños-container");
 
+// Se generan constantes para el GET de Patentes
 const getVehiculos = () => firebase.firestore().collection("Vehiculos").get();
 const ongetVehiculos = (callback) => firebase.firestore().collection("Vehiculos").onSnapshot(callback);
 const getPatente = (id) => firebase.firestore().collection("Vehiculos").doc(id).get();
 const updatePatente = (id, updatePatente) => firebase.firestore().collection("Vehiculos").doc(id).update(updatePatente);
 
+// Se generan constantes para el GET de Dueños
+const getDueños = () => firebase.firestore().collection("Dueños").get();
+const ongetDueños = (callback) => firebase.firestore().collection("Dueños").onSnapshot(callback);
+const getDueños_data = (id) => firebase.firestore().collection("Dueños").doc(id).get();
+const updateDueños = (id, updateDueños) => firebase.firestore().collection("Dueños").doc(id).update(updateDueños);
 
+
+//Formulario para CREAR DUEÑO
 if (crear_dueño_form != null){
     let d;
     crear_dueño_form.addEventListener("submit", async(e)=>{ 
@@ -52,7 +64,53 @@ if (crear_dueño_form != null){
     });
 };
 
+//Formulario para obtener patente al ingresar un vehiculo
+if (ingreso_form != null){
+    let d;
+    ingreso_form.addEventListener("submit", async(e)=>{ 
+        e.preventDefault();
+        if(document.getElementById("patente").value != "")
+        {
+            let fecha = new Date();
+            var dia = fecha.getDate();
+            var mes = fecha.getMonth();
+            var year = fecha.getFullYear();
+            var h = fecha.getHours();
+            var m = fecha.getMinutes();
+            if (h == 24){
+                h = 0;
+            } else if(h > 12){
+                h = h - 0;
+            }
+            if(h<10){
+                h = "0"+ h;
+            }
+            if(m<10){
+                m = "0"+ m;
+            }
+            
+            let patente = document.getElementById("patente").value;
+            let fecha_total = dia+ "/"+ (mes+1) + "/" + year;
+            let ingreso = h + ":" + m;
+            let salida = "--:--";
 
+            firebase.firestore().collection("Vehiculos").doc().set({
+                patente,
+                fecha_total,
+                ingreso,
+                salida,
+            })
+            alert("Se ha ingresado el vehiculo correctamente");
+            ingreso_form.reset();
+
+        }else
+        {
+            console.log("Debe completar los campos...");
+        }
+    });
+};
+
+//GET Formulario para Ingreso y Salida de Vehiculos 
 window.addEventListener('DOMContentLoaded', async(e) => {
     ongetVehiculos( (querySnapshot) =>{
 
@@ -104,7 +162,7 @@ window.addEventListener('DOMContentLoaded', async(e) => {
                     await updatePatente(e.target.dataset.id, {
                         salida:out,
                     })
-                    alert("Se agrego la salida correctamente")
+                    alert("Vehiculo Salio Correctamente")
 
                 }catch (error) {
                     console.log(error);
@@ -112,51 +170,95 @@ window.addEventListener('DOMContentLoaded', async(e) => {
             })
         })
     })
-})
+});
 
-if (ingreso_form != null){
-    let d;
-    ingreso_form.addEventListener("submit", async(e)=>{ 
-        e.preventDefault();
-        if(document.getElementById("patente").value != "")
-        {
-            let fecha = new Date();
-            var dia = fecha.getDate();
-            var mes = fecha.getMonth();
-            var year = fecha.getFullYear();
-            var h = fecha.getHours();
-            var m = fecha.getMinutes();
-            if (h == 24){
-                h = 0;
-            } else if(h > 12){
-                h = h - 0;
-            }
-            if(h<10){
-                h = "0"+ h;
-            }
-            if(m<10){
-                m = "0"+ m;
-            }
+
+//GET Formulario para obtener todos los dueños
+window.addEventListener('DOMContentLoaded', async(e) => {
+    e.preventDefault();
+    ongetDueños( (querySnapshot) =>{
+            Dueños_Container.innerHTML = '';
+
+            querySnapshot.forEach(doc =>{
+
+                const dueños_db = doc.data();
+                Dueños_Container.innerHTML += 
+                    `<div class="card card-body mt-2 border-primary" >
+                                <tr>
+                                <td>${dueños_db.nombre} </td>
+                                <td>${dueños_db.apellido} </td>
+                                <td>${dueños_db.patente} </td>
+                                <td>${dueños_db.telefono} </td>
+                                <td>${dueños_db.descripcion}</td>
+                                </tr>
+                            </tbody>
+                        </table>        
+                    </div>`;
+            });
+        });
+});
+
+//GET Formulario para obtener dueños filtrados (Busqueda)
+window.addEventListener('DOMContentLoaded', async(e) => {
+    ongetDueños( (querySnapshot) =>{
+        buscar_dueño_form.addEventListener("submit", async(e)=>{ 
+            e.preventDefault();
+            Dueños_Container.innerHTML = '';
+
+            querySnapshot.forEach(doc =>{
+               
+                const dueños_db = doc.data();
+                console.log(dueños_db.patente);
+                patente_d = document.getElementById("buscador_dueño").value;
+                
+                if(dueños_db.patente == patente_d ){
+                    Dueños_Container.innerHTML += 
+                    `<div class="card card-body mt-2 border-primary" >
+                                <tr>
+                                <td>${dueños_db.nombre} </td>
+                                <td>${dueños_db.apellido} </td>
+                                <td>${dueños_db.patente} </td>
+                                <td>${dueños_db.telefono} </td>
+                                <td>${dueños_db.descripcion}</td>
+                                </tr>
+                            </tbody>
+                        </table>        
+                    </div>`;
+                }
             
-            let patente = document.getElementById("patente").value;
-            let fecha_total = dia+ "/"+ (mes+1) + "/" + year;
-            let ingreso = h + ":" + m;
-            let salida = "--:--";
-
-            firebase.firestore().collection("Vehiculos").doc().set({
-                patente,
-                fecha_total,
-                ingreso,
-                salida,
-            })
-            alert("Se ha creado el dueño correctamente");
-            ingreso_form.reset();
-
-        }else
-        {
-            console.log("Debe completar los campos...");
-        }
+            });
+        });
+        buscar_dueño_form.reset();
     });
-};
+});
 
+//GET Formulario para obtener patentes filtrados (Busqueda)
+window.addEventListener('DOMContentLoaded', async(e) => {
+    ongetVehiculos( (querySnapshot) =>{
+        buscar_salida_form.addEventListener("submit", async(e)=>{ 
+            e.preventDefault();
+            Vehiculos_Container.innerHTML = '';
 
+            querySnapshot.forEach(doc => 
+            {
+                
+                const vehiculo_db = doc.data();
+                const patente_s = document.getElementById("buscador_salida").value;
+                
+                if(vehiculo_db.patente  == patente_s && vehiculo_db.salida == "--:--") {
+                    Vehiculos_Container.innerHTML += 
+                    `<div class="card card-body mt-2 border-primary" >
+                                <tr>
+                                <td>${vehiculo_db.patente} </td>
+                                <td>${vehiculo_db.ingreso} </td>
+                                <td><button class="w3-button w3-white w3-padding-large w3-hover-red btn-salida" style="margin:0px 70px 0px 100px" data-id="${doc.id}"> Salida</td>
+                                </tr>
+                            </tbody>
+                        </table>        
+                        
+                    </div>`;
+                }
+            });
+        });
+    });
+});
